24aee3912a3243837798c6a1ba9f868cb75f7265
Isaias <lawicki76@gmail.com>
2023-06-27 18:17:18 -0300

    update

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -86,54 +86,54 @@ export default function Hero() {
           </ul>
         </nav>
         {/* Menu hamburgesa mobile */}
-        <div className="flex items-center justify-between py-8 border-gray-400">
-          <nav className='mobile-menu'>
-            <section className="flex lg:hidden">
-              <div
-                className="space-y-2"
-                onClick={() => setIsNavOpen((prev) => !prev)}
-              >
-                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-              </div>
-
-              <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+        <div className="flex items-center justify-between border-gray-400 py-8">
+            <nav className='mobile-menu'>
+              <section className="MOBILE-MENU flex lg:hidden">
                 <div
-                  className="absolute top-0 right-0 px-8 py-8"
-                  onClick={() => setIsNavOpen(false)}
+                  className="HAMBURGER-ICON space-y-2"
+                  onClick={() => setIsNavOpen((prev) => !prev)}
                 >
-                  <svg
-                    className="w-8 h-8 text-gray-600"
-                    viewBox="0 0 24 24"
-                    fill="none"
-                    stroke="currentColor"
-                    strokeWidth="2"
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                  >
-                    <line x1="18" y1="6" x2="6" y2="18" />
-                    <line x1="6" y1="6" x2="18" y2="18" />
-                  </svg>
+                  <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+                  <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+                  <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
                 </div>
-                <ul className="flex flex-col items-center justify-between min-h-[250px]">
-                  <li className="my-8 uppercase border-gray-400">
-                    <a href="/">Quienes somos</a>
-                  </li>
-                  <li className="my-8 uppercase border-gray-400">
-                    <a href="/">Ministerios</a>
-                  </li>
-                  <li className="my-8 uppercase border-gray-400">
-                    <a href="/">Edades</a>
-                  </li>
-                  <li className="my-8 uppercase border-gray-400">
-                    <a href="/">Contacto</a>
-                  </li>
-                </ul>
-              </div>
-            </section>
-          </nav>
-        </div>
+
+                <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                  <div
+                    className="absolute top-0 right-0 px-8 py-8"
+                    onClick={() => setIsNavOpen(false)}
+                  >
+                    <svg
+                      className="h-8 w-8 text-gray-600"
+                      viewBox="0 0 24 24"
+                      fill="none"
+                      stroke="currentColor"
+                      strokeWidth="2"
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                    >
+                      <line x1="18" y1="6" x2="6" y2="18" />
+                      <line x1="6" y1="6" x2="18" y2="18" />
+                    </svg>
+                  </div>
+                  <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                    <li className="border-gray-400 my-8 uppercase">
+                      <a href="/">Quienes somos</a>
+                    </li>
+                    <li className="border-gray-400 my-8 uppercase">
+                      <a href="/">Ministerios</a>
+                    </li>
+                    <li className="border-gray-400 my-8 uppercase">
+                      <a href="/">Edades</a>
+                    </li>
+                    <li className="border-gray-400 my-8 uppercase">
+                      <a href="/">Contacto</a>
+                    </li>
+                  </ul>
+                </div>
+              </section>
+            </nav>
+          </div>
         {/* Menu hamburgesa mobile */}
       </header>
       <div className={cn('absolute inset-0 transition-opacity duration-300 ease-in', mount ? 'opacity-100' : 'opacity-0')}>
