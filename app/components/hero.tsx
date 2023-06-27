b2e44c1278a9f0ab7e3668f1fb0afaf663aa58b1
Benjamin Flores <beneliflo@gmail.com>
2023-06-27 15:30:51 -0300

    moved menu mobile

--- app/components/hero.tsx
+++ app/components/hero.tsx
@@ -16,6 +16,8 @@ import heroOne from '../../public/hero.jpg'
 import heroTwo from '../../public/hero2.jpg'
 
 export default function Hero() {
+  const [isNavOpen, setIsNavOpen] = useState(false);
+
   const [mount, setMount] = useState(false);
 
   const [sliderRef, instanceRef] = useKeenSlider(
@@ -83,6 +85,56 @@ export default function Hero() {
             </li>
           </ul>
         </nav>
+        {/* Menu hamburgesa mobile */}
+        <div className="flex items-center justify-between py-8 border-gray-400">
+          <nav className='mobile-menu'>
+            <section className="flex MOBILE-MENU lg:hidden">
+              <div
+                className="space-y-2 HAMBURGER-ICON"
+                onClick={() => setIsNavOpen((prev) => !prev)}
+              >
+                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+                <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
+              </div>
+
+              <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
+                <div
+                  className="absolute top-0 right-0 px-8 py-8"
+                  onClick={() => setIsNavOpen(false)}
+                >
+                  <svg
+                    className="w-8 h-8 text-gray-600"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                  >
+                    <line x1="18" y1="6" x2="6" y2="18" />
+                    <line x1="6" y1="6" x2="18" y2="18" />
+                  </svg>
+                </div>
+                <ul className="flex flex-col items-center justify-between min-h-[250px]">
+                  <li className="my-8 uppercase border-gray-400">
+                    <a href="/">Quienes somos</a>
+                  </li>
+                  <li className="my-8 uppercase border-gray-400">
+                    <a href="/">Ministerios</a>
+                  </li>
+                  <li className="my-8 uppercase border-gray-400">
+                    <a href="/">Edades</a>
+                  </li>
+                  <li className="my-8 uppercase border-gray-400">
+                    <a href="/">Contacto</a>
+                  </li>
+                </ul>
+              </div>
+            </section>
+          </nav>
+        </div>
+        {/* Menu hamburgesa mobile */}
       </header>
       <div className={cn('absolute inset-0 transition-opacity duration-300 ease-in', mount ? 'opacity-100' : 'opacity-0')}>
         <div ref={sliderRef} className="h-full keen-slider">
--- app/page.tsx
+++ app/page.tsx
@@ -14,60 +14,10 @@ export default function Home() {
 
   const mapEmbedCode = '<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3950.5866264193533!2d-58.51235886463723!3d-34.88144292619042!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bcd76848809fd5%3A0xa4dcb81c015f76f2!2sIglesia%20el%20encuentro!5e0!3m2!1ses-419!2sar!4v1687666073992!5m2!1ses-419!2sar" width="100%" height="600" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>';
   
-  const [isNavOpen, setIsNavOpen] = useState(false);
+  
 
   return (
     <main>
-    {/* Menu hamburgesa mobile */}
-    <div className="flex items-center justify-between border-gray-400 py-8">
-        <nav className='mobile-menu'>
-          <section className="MOBILE-MENU flex lg:hidden">
-            <div
-              className="HAMBURGER-ICON space-y-2"
-              onClick={() => setIsNavOpen((prev) => !prev)}
-            >
-              <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-              <span className="block h-0.5 w-8 animate-pulse bg-white"></span>
-            </div>
-
-            <div className={isNavOpen ? "showMenuNav" : "hideMenuNav"}>
-              <div
-                className="absolute top-0 right-0 px-8 py-8"
-                onClick={() => setIsNavOpen(false)}
-              >
-                <svg
-                  className="h-8 w-8 text-gray-600"
-                  viewBox="0 0 24 24"
-                  fill="none"
-                  stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                >
-                  <line x1="18" y1="6" x2="6" y2="18" />
-                  <line x1="6" y1="6" x2="18" y2="18" />
-                </svg>
-              </div>
-              <ul className="flex flex-col items-center justify-between min-h-[250px]">
-                <li className="border-gray-400 my-8 uppercase">
-                  <a href="/">Quienes somos</a>
-                </li>
-                <li className="border-gray-400 my-8 uppercase">
-                  <a href="/">Ministerios</a>
-                </li>
-                <li className="border-gray-400 my-8 uppercase">
-                  <a href="/">Edades</a>
-                </li>
-                <li className="border-gray-400 my-8 uppercase">
-                  <a href="/">Contacto</a>
-                </li>
-              </ul>
-            </div>
-          </section>
-        </nav>
-      </div>
-      {/* Menu hamburgesa mobile */}
       <Hero />
       {/* Palabra Rhema section */}
       <div className="container text-center py-14 md:py-20">
