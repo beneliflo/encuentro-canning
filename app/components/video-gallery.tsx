d44f3fce449cfeb2f574c2c59d780ee9f432678f
Benjamin Flores <beneliflo@gmail.com>
2023-06-28 22:09:08 -0300

    update

--- app/components/video-gallery.tsx
+++ app/components/video-gallery.tsx
@@ -4,7 +4,16 @@ import { useState, useEffect } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
 
-import { fetchYoutubeData } from '@/app/api/youtube/route'
+const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
+
+export async function fetchYoutubeData() {
+  const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=50&playlistId=PL_3tjwhL5Q7kDCCE09JBFqeAZj5cRrib7&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
+  }
+ 
+  return res.json()
+}
 
 interface VideoItem {
   id: string;
