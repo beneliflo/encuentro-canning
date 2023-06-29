6b28161ea78dee9608beefed8aa1e846e27e4370
Benjamin Flores <beneliflo@gmail.com>
2023-06-29 01:38:54 -0300

    update

--- app/components/video-gallery.tsx
+++ app/components/video-gallery.tsx
@@ -7,15 +7,12 @@ import Link from 'next/link'
 const YOUTUBE_PLAYLIST_ITEMS_API = 'https://www.googleapis.com/youtube/v3/playlistItems';
 
 export async function fetchYoutubeData() {
-  try {
-    const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=50&playlistId=PL_3tjwhL5Q7kDCCE09JBFqeAZj5cRrib7&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`);
-    if (!res.ok) {
-      throw new Error(`Failed to fetch data: ${res.status} ${res.statusText}`);
-    }
-    return res.json();
-  } catch (error: any) {
-    throw new Error(`Failed to fetch data: ${error.message}`);
+  const res = await fetch(`${YOUTUBE_PLAYLIST_ITEMS_API}?part=snippet&maxResults=50&playlistId=PL_3tjwhL5Q7kDCCE09JBFqeAZj5cRrib7&key=${process.env.NEXT_PUBLIC_YOUTUBE_API_KEY}`);
+  if (!res.ok) {
+    throw new Error('Failed to fetch data')
   }
+ 
+  return res.json()
 }
 
 interface VideoItem {
