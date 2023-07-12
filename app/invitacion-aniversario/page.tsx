083f3d7316a3971adb79e5c03aa47ac604e961b7
Benjamin Flores <beneliflo@gmail.com>
2023-07-12 02:14:48 -0300

    fix mobile

--- app/invitacion-aniversario/page.tsx
+++ app/invitacion-aniversario/page.tsx
@@ -76,20 +76,20 @@ export default function Invitation() {
           <Image src={photoTop} alt='photo top' loading='eager' />
           <Image src={goldTop} alt='gold top' loading='eager' className="absolute -bottom-1 max-h-[30px] w-full" />
         </div>
-        <div className="flex flex-col px-10 py-10">
-          <h3 className={cn('text-6xl font-bold', snellRoundhand.className)}>Te invitamos a:</h3>
-          <h2 className={cn('text-[6.25rem] font-normal text-[#DFA23E] leading-[0.5] mt-12 mb-24 text-right', snellRoundhand.className)}>
-            <p className="block transform -translate-x-44">Gran</p>
+        <div className="flex flex-col p-6 sm:p-10">
+          <h3 className={cn('text-3xl sm:text-5xl lg:text-6xl font-bold', snellRoundhand.className)}>Te invitamos a:</h3>
+          <h2 className={cn('text-5xl sm:text-8xl lg:text-[6.25rem] font-normal text-[#DFA23E] sm:leading-[0.5] leading-[0.55] mt-8 mb-12 sm:mt-12 sm:mb-24 text-center lg:text-right lg:mr-16', snellRoundhand.className)}>
+            <p className="block transform lg:-translate-x-44">Gran</p>
             Inaguración
           </h2>
-          <h1 className={cn('uppercase text-[8.45rem] font-bold leading-[0.85] mb-12', theSeasons.className)}>
+          <h1 className={cn('uppercase text-5xl sm:text-8xl lg:text-[8.45rem] font-bold leading-[0.85] mb-12 text-center lg:text-left', theSeasons.className)}>
             <p className="block transform lg:translate-x-20">Encuentro</p>
             <p className="block">Canning</p>
           </h1>
           <div className="flex flex-col items-center max-w-2xl mx-auto text-center">
-            <p className={cn('text-3xl', objectSans.className)}>Compartí con nosotros la alegría de ver el <b>sueño de Dios</b> hecho realidad. <br /><b>2 Corintios 9:8</b></p>
-            <Image src={split} alt="split" loading='eager' className="my-4" />
-            <p className={cn('text-4xl font-bold', openSauce.className)}>VIERNES 25/08 | 19hs</p>
+            <p className={cn('text-xl sm:text-3xl', objectSans.className)}>Compartí con nosotros la alegría de ver el <b>sueño de Dios</b> hecho realidad. <br /><b>2 Corintios 9:8</b></p>
+            <Image src={split} alt="split" loading='eager' className="max-w-[200px] sm:max-w-full my-6 sm:my-10" />
+            <p className={cn('text-2xl sm:text-4xl font-bold', openSauce.className)}>VIERNES 25/08 | 19hs</p>
           </div>
         </div>
         <div className="relative">
