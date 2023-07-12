c360e8913cd937c922c5fc929d1570b1cbf8c6b7
Benjamin Flores <beneliflo@gmail.com>
2023-07-12 20:31:34 -0300

    test og

--- app/invitacion-aniversario/page.tsx
+++ app/invitacion-aniversario/page.tsx
@@ -2,9 +2,16 @@ import { Metadata } from 'next'
 
 import Invitation from './components/invitation'
 
+export const openGraphImage = { images: ['/invitation/bg.jpg'] }
+
 export const metadata: Metadata = {
   title: 'Invitacion Aniversario',
-  description: 'Invitacion especial a nuestro primer aniversario'
+  description: 'Invitacion especial a nuestro primer aniversario',
+  openGraph: {
+    ...openGraphImage,
+    title: 'Acme',
+    description: 'Acme is a...',
+  },
 }
 
 export default function InvitationPage() {
