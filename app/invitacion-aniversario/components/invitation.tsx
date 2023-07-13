eb27b404678bb2d035040adb61af9cee74ca7dcf
Benjamin Flores <beneliflo@gmail.com>
2023-07-12 22:13:35 -0300

    add animation

--- app/invitacion-aniversario/components/invitation.tsx
+++ app/invitacion-aniversario/components/invitation.tsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react'
 import cn from 'classnames'
 import Image from 'next/image'
 import Link from 'next/link'
+import { Fade } from "react-awesome-reveal";
 import localFont from 'next/font/local'
 import { Titillium_Web } from 'next/font/google'
 
@@ -123,50 +124,74 @@ export default function Invitation() {
       {letter &&
         <div className="max-w-[992px] mx-auto animate-slideFadeUp p-4">
           <div className="overflow-hidden bg-white rounded-2xl">
-            <div className="relative z-10">
-              <Image src={photoTop} alt='photo top' loading='eager' />
-              <Image src={goldTop} alt='gold top' loading='eager' className="absolute -bottom-1 max-h-[20px] w-full" />
-            </div>
+            <Fade triggerOnce>
+              <div className="relative z-10">
+                <Image src={photoTop} alt='photo top' loading='eager' />
+                <Image src={goldTop} alt='gold top' loading='eager' className="absolute -bottom-1 max-h-[20px] w-full" />
+              </div>
+            </Fade>
             <div className="relative flex flex-col p-6 sm:p-10 bg-[url('/invitation/bg-invitation.png')] bg-cover bg-no-repeat bg-center">
-              <h3 className={cn('text-3xl sm:text-5xl lg:text-6xl font-bold', snellRoundhand.className)}>Te invitamos a:</h3>
-              <h2 className={cn('text-5xl sm:text-8xl lg:text-[6rem] font-normal text-[#B48D48] sm:leading-[0.5] leading-[0.55] mt-8 mb-12 sm:mt-12 sm:mb-24 text-center lg:text-right lg:mr-16', snellRoundhand.className)}>
-                <p className="block transform lg:-translate-x-44">Gran</p>
-                Inaguración
-              </h2>
-              <h1 className={cn('uppercase text-5xl sm:text-8xl lg:text-[7rem] font-bold leading-[0.85] text-center relative z-10 mb-12', theSeasons.className)}>
-                <p className="block transform lg:translate-x-20">Encuentro</p>
-                <p className="block transform lg:-translate-x-32">Canning</p>
-              </h1>
+              <Fade triggerOnce direction='left'>
+                <h3 className={cn('text-3xl sm:text-5xl lg:text-6xl font-bold', snellRoundhand.className)}>Te invitamos a:</h3>
+              </Fade>
+              <Fade triggerOnce direction='right'>
+                <h2 className={cn('text-5xl sm:text-8xl lg:text-[6rem] font-normal text-[#B48D48] sm:leading-[0.5] leading-[0.55] mt-8 mb-12 sm:mt-12 sm:mb-24 text-center lg:text-right lg:mr-16', snellRoundhand.className)}>
+                  <p className="block transform lg:-translate-x-44">Gran</p>
+                  Inaguración
+                </h2>
+              </Fade>
+              <Fade triggerOnce direction='up'>
+                <h1 className={cn('uppercase text-5xl sm:text-8xl lg:text-[7rem] font-bold leading-[0.85] text-center relative z-10 mb-12', theSeasons.className)}>
+                  <p className="block transform lg:translate-x-20">Encuentro</p>
+                  <p className="block transform lg:-translate-x-32">Canning</p>
+                </h1>
+              </Fade>
               <div className="flex flex-col items-center max-w-2xl mx-auto text-center">
-                <p className={cn('text-xl sm:text-3xl', objectSans.className)}>Compartí con nosotros la alegría de ver el <b>sueño de Dios</b> hecho realidad. <br /><b>2 Corintios 9:8</b></p>
-                <Image src={split} alt="split" loading='eager' className="max-w-[200px] sm:max-w-[340px] my-6 sm:my-10" />
-                <p className={cn('text-2xl sm:text-4xl font-bold', openSauce.className)}>VIERNES 25/08 | 19hs</p>
+                <Fade triggerOnce direction='up'>
+                  <p className={cn('text-xl sm:text-3xl', objectSans.className)}>Compartí con nosotros la alegría de ver el <b>sueño de Dios</b> hecho realidad. <br /><b>2 Corintios 9:8</b></p>
+                </Fade>
+                <Fade triggerOnce direction='up'>
+                  <Image src={split} alt="split" loading='eager' className="max-w-[200px] sm:max-w-[340px] my-6 sm:my-10" />
+                  <p className={cn('text-2xl sm:text-4xl font-bold', openSauce.className)}>VIERNES 25/08 | 19hs</p>
+                </Fade>
               </div>
             </div>
-            <div className="relative">
-              <Image src={photoBottom} alt='photo bottom' loading='eager' />
-              <Image src={goldBottom} alt='gold bottom' loading='eager' className="absolute -top-1 max-h-[20px] w-full" />
-            </div>
+            <Fade triggerOnce>
+              <div className="relative">
+                <Image src={photoBottom} alt='photo bottom' loading='eager' />
+                <Image src={goldBottom} alt='gold bottom' loading='eager' className="absolute -top-1 max-h-[20px] w-full" />
+              </div>
+            </Fade>
             {/* Confirmation */}
             <div className="relative bg-[url('/invitation/bg-confirmation.png')] bg-cover bg-top bg-no-repeat p-6 sm:p-10 flex flex-col">
-              <h2 className={cn('text-5xl sm:text-6xl font-normal text-[#B48D48] sm:leading-[0.5] leading-[0.55] pt-8 pb-12 sm:pt-12 sm:pb-24 text-center lg:text-left', snellRoundhand.className)}>
-                <p className="block transform lg:translate-x-24">Gran</p>
-                Inaguración
-              </h2>
-              <p className={cn('text-xl sm:text-4xl font-bold text-center max-w-2xl mx-auto mb-10', objectSans.className)}>SE REQUIERE CONFIRMACIÓN DE ASISTENCIA PREVIA</p>
-              <div className="flex flex-col gap-4">
-                <Link className={cn(titillium_web.className, 'transition bg-[#DFA23E] hover:bg-[#B48D48] px-4 py-2 rounded-full max-w-max font-bold text-base border border-[#B48D48] text-white mx-auto')} href='https://wa.me/5491168194422?text=Confirmo%20mi%20asistencia'>CONFIRME SU PRESENCIA AQUÍ</Link>
-                <Link className={cn(titillium_web.className, 'transition bg-[#DFA23E] hover:bg-[#B48D48] px-4 py-2 rounded-full max-w-max font-bold text-base border border-[#B48D48] text-white mx-auto')} href='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MDlndTVyZnZlZzE5azhlN2k3N2k4cGZyZHEgZWxlbmN1ZW50cm9jYW5uaW5nLm9rQG0&tmsrc=elencuentrocanning.ok%40gmail.com'>AÑADIR A CALENDARIO</Link>
+              <Fade triggerOnce direction='left'>
+                <h2 className={cn('text-5xl sm:text-6xl font-normal text-[#B48D48] sm:leading-[0.5] leading-[0.55] pt-8 pb-12 sm:pt-12 sm:pb-24 text-center lg:text-left', snellRoundhand.className)}>
+                  <p className="block transform lg:translate-x-24">Gran</p>
+                  Inaguración
+                </h2>
+              </Fade>
+              <Fade triggerOnce direction='up'>
+                <p className={cn('text-xl sm:text-4xl font-bold text-center max-w-2xl mx-auto mb-10', objectSans.className)}>SE REQUIERE CONFIRMACIÓN DE ASISTENCIA PREVIA</p>
+              </Fade>
+              <div className="flex flex-col justify-center gap-4 text-center">
+                <Fade triggerOnce cascade direction='up'>
+                  <Link className={cn(titillium_web.className, 'inline-block transition bg-[#DFA23E] hover:bg-[#B48D48] px-4 py-2 rounded-full max-w-max font-bold text-base border border-[#B48D48] text-white mx-auto')} href='https://wa.me/5491168194422?text=Confirmo%20mi%20asistencia'>CONFIRME SU PRESENCIA AQUÍ</Link>
+                  <Link className={cn(titillium_web.className, 'inline-block transition bg-[#DFA23E] hover:bg-[#B48D48] px-4 py-2 rounded-full max-w-max font-bold text-base border border-[#B48D48] text-white mx-auto')} href='https://calendar.google.com/calendar/event?action=TEMPLATE&tmeid=MDlndTVyZnZlZzE5azhlN2k3N2k4cGZyZHEgZWxlbmN1ZW50cm9jYW5uaW5nLm9rQG0&tmsrc=elencuentrocanning.ok%40gmail.com'>AÑADIR A CALENDARIO</Link>
+                </Fade>
               </div>
               <div className="flex flex-col gap-6 mt-20">
-                <p className={cn('text-xl sm:text-3xl font-bold text-center uppercase', objectSans.className)}>Hipócrates 3320, Canning, Ezeiza</p>
-                <div className="overflow-hidden rounded-2xl" dangerouslySetInnerHTML={{ __html: mapEmbedCode }} />
+                <Fade triggerOnce cascade direction='up'>
+                  <p className={cn('text-xl sm:text-3xl font-bold text-center uppercase', objectSans.className)}>Hipócrates 3320, Canning, Ezeiza</p>
+                  <div className="overflow-hidden rounded-2xl" dangerouslySetInnerHTML={{ __html: mapEmbedCode }} />
+                </Fade>
               </div>
-              <p className={cn('text-2xl sm:text-4xl font-normal text-center mt-20 max-w-lg mx-auto mb-20 border-double border-4 border-[#DFA23E] py-4 rounded-lg', titillium_web.className)}>CÓDIGO DE VESTIMENTA ELEGANTE SPORT</p>
-              <h1 className={cn('uppercase text-5xl sm:text-8xl font-bold leading-[0.85] text-center relative z-10 mb-20', theSeasons.className)}>
-                <p className="block transform lg:translate-x-20">Encuentro</p>
-                <p className="block transform lg:-translate-x-32">Canning</p>
-              </h1>
+              <Fade triggerOnce cascade>
+                <p className={cn('text-2xl sm:text-4xl font-normal text-center mt-20 max-w-lg mx-auto mb-20 border-double border-4 border-[#DFA23E] py-4 rounded-lg', titillium_web.className)}>CÓDIGO DE VESTIMENTA ELEGANTE SPORT</p>
+                <h1 className={cn('uppercase text-5xl sm:text-8xl font-bold leading-[0.85] text-center relative z-10 mb-20', theSeasons.className)}>
+                  <p className="block transform lg:translate-x-20">Encuentro</p>
+                  <p className="block transform lg:-translate-x-32">Canning</p>
+                </h1>
+              </Fade>
               <Image src={confirmationBottom} alt='confirmation bottom' loading='eager' className="absolute inset-x-0 bottom-0" />
             </div>
           </div>
