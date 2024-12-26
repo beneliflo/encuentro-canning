42064fb0ea2ec0d71d264c427ff2f5ff551dc14f
Benjamin Flores <beneliflo@gmail.com>
2024-12-26 03:36:48 -0300

    add hero

--- app/preview/page.tsx
+++ app/preview/page.tsx
@@ -1,10 +1,12 @@
-import cn from 'classnames'
-import Header from './components/header'
+import cn from 'classnames';
+import Header from './components/header';
+import Hero from './components/homepage/hero';
 
 export default function Home() {
   return (
     <main>
       <Header />
+      <Hero />
     </main>
-  )
+  );
 }
