646ef6250a5b6ebf2c80113d49855f3aa0541073
Benjamin Flores <beneliflo@gmail.com>
2024-12-27 17:09:47 -0300

    update video hero (#5)
    * update video hero
    
    * test
    
    * test
    
    * update
    
    * update
    
    * fix
    
    * fix

--- app/preview/components/homepage/hero.tsx
+++ app/preview/components/homepage/hero.tsx
@@ -1,23 +1,93 @@
 'use client';
 
+import React, { useState, useEffect } from 'react';
 import Image from 'next/image';
-import Link from 'next/link';
-import cn from 'classnames';
 import dynamic from 'next/dynamic';
+import cn from 'classnames';
 
 const ReactPlayer = dynamic(() => import('react-player'), { ssr: false });
 
 export default function Hero() {
+  const [isVideoPlaying, setIsVideoPlaying] = useState(false);
+  const [currentWord, setCurrentWord] = useState('FAMILIA');
+  const [currentSubtitle, setCurrentSubtitle] = useState(
+    'un lugar donde pertenecer'
+  );
+  const [videoProgress, setVideoProgress] = useState(0);
+
+  const words = ['FAMILIA', 'PASIÓN', 'COMPASIÓN', 'PROPÓSITO', 'PODER'];
+  const subtitles = [
+    'un lugar donde pertenecer',
+    'el encuentro más maravilloso',
+    'amando al estilo Jesús',
+    'discípulos que hacen discípulos',
+    'un milagro te está esperando',
+  ];
+
+  const appearAtSeconds = [0, 19, 37, 54, 66];
+  const disappearAtSeconds = [19, 37, 54, 66, 77];
+
+  useEffect(() => {
+    if (isVideoPlaying) {
+      for (let i = 0; i < words.length; i++) {
+        if (
+          videoProgress >= appearAtSeconds[i] &&
+          videoProgress < disappearAtSeconds[i]
+        ) {
+          setCurrentWord(words[i]);
+          setCurrentSubtitle(subtitles[i]);
+          return;
+        }
+      }
+    }
+  }, [videoProgress]);
+
+  console.log(videoProgress);
+
   return (
-    <div>
-      <ReactPlayer
-        url="/hero-with-values.mp4"
-        playing
-        muted
-        loop
-        width="100%"
-        height="auto"
-      />
+    <div className="relative flex items-center justify-center overflow-hidden h-dvh max-h-[600px] md:max-h-none md:min-h-[880px]">
+      {/* Placeholder */}
+      {!isVideoPlaying && (
+        <div className="absolute inset-0 flex items-center justify-center bg-black">
+          <Image
+            src="/hero-video-placeholder.png"
+            width={1920}
+            height={1080}
+            alt="Loading..."
+            className="object-cover w-full h-full"
+          />
+        </div>
+      )}
+
+      {/* Video */}
+      <div
+        className={cn(
+          'absolute inset-0',
+          isVideoPlaying ? 'opacity-100' : 'opacity-0'
+        )}
+      >
+        <ReactPlayer
+          className="hero-player"
+          url="/hero-video.mp4"
+          playing
+          muted
+          loop
+          width="100%"
+          height="100%"
+          onReady={() => setIsVideoPlaying(true)}
+          onProgress={({ playedSeconds }) => setVideoProgress(playedSeconds)}
+        />
+      </div>
+
+      {/* Textos intercalados */}
+      <div className="container absolute z-10 text-white">
+        <p className="font-bold text-5xl md:text-8xl lg:text-9xl xl:text-[10rem] leading-none font-pragmatica">
+          {currentWord}
+        </p>
+        <p className="text-2xl font-light tracking-wider md:pl-1.5 md:text-5xl xl:pl-2 xl:text-[3.3rem]">
+          {currentSubtitle}
+        </p>
+      </div>
     </div>
   );
 }
--- app/preview/components/homepage/nuestros-encuentros.tsx
+++ app/preview/components/homepage/nuestros-encuentros.tsx
@@ -6,13 +6,13 @@ import cn from 'classnames';
 
 export default function NuestrosEncuentros() {
   return (
-    <div className="py-12 bg-dark">
+    <div className="py-10 md:py-20 bg-dark">
       <div className="container">
         <h2 className="font-bold font-pragmatica text-4xl md:text-8xl lg:text-[107px] md:leading-[110px] text-hueso uppercase">
           <p>Nuestros</p>
           <p className="text-right">Encuentros</p>
         </h2>
-        <div className="mt-24">
+        <div className="mt-10 md:mt-24">
           <ul className="grid gap-4 font-bold text-center md:grid-cols-3 font-pragmatica text-hueso">
             <li className="relative aspect-[363/260]">
               <Link href="#">
