4180caab96909cd158f40c6e13712f7eb13c9e2a
Benjamin Flores <benjamin.flores@scale.com>
2025-02-24 20:59:02 -0300

    fix SEO

--- app/invitacion-aniversario/layout.tsx
+++ app/invitacion-aniversario/layout.tsx
@@ -1,17 +1,32 @@
-import { Metadata } from 'next'
-// import Script from 'next/script'
+import type { Metadata } from 'next'
 import '@/app/globals.css'
 import { Inter } from 'next/font/google'
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
-  title: 'Invitación 8vo Aniversario | Encuentro Canning',
+  title: 'Invitación 8vo Aniversario',
   description: 'Invitación al 8vo Aniversario de Encuentro Canning',
   openGraph: {
-    images: 'https://encuentrocanning.org/invitation/og-image.png',
+    title: 'Invitación 8vo Aniversario | Encuentro Canning',
+    description: 'Invitación al 8vo Aniversario de Encuentro Canning',
+    url: 'https://encuentrocanning.org/invitacion-aniversario',
+    siteName: 'Encuentro Canning',
+    images: [
+      {
+        url: 'https://encuentrocanning.org/invitation/og-image.png',
+        width: 1200,
+        height: 630,
+        alt: 'Invitación 8vo Aniversario Encuentro Canning',
+      }
+    ],
+    locale: 'es_AR',
+    type: 'website',
   },
   twitter: {
+    card: 'summary_large_image',
+    title: 'Invitación 8vo Aniversario | Encuentro Canning',
+    description: 'Invitación al 8vo Aniversario de Encuentro Canning',
     images: ['https://encuentrocanning.org/invitation/og-image.png'],
   },
 }
@@ -22,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode
 }) {
   return (
-    <html lang="en">
+    <html lang="es">
       <link rel="canonical" href="https://encuentrocanning.org/invitacion-aniversario"/>
       <body className={inter.className}>{children}</body>
     </html>
