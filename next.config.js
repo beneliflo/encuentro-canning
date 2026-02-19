567d43d948e70dc86cdf3ddc32a15bcef2aaba9c
Benjamin Flores <beneliflo@gmail.com>
2026-02-19 15:24:37 -0300

    chore: remove static export, enable image optimization and dynamic sitemap

--- next.config.js
+++ next.config.js
@@ -2,9 +2,8 @@
 const isProd = process.env.NODE_ENV === 'production'
 
 const nextConfig = {
-  output: 'export',
   images: { 
-    unoptimized: true
+    unoptimized: false
   },
   assetPrefix: isProd ? '' : undefined,
 }
