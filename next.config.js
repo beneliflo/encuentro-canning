438762efd4689da1c24cfe8492604a8383d06788
Benjamin Flores <beneliflo@gmail.com>
2026-03-24 23:36:30 -0300

    feat: remove congrats page

--- next.config.js
+++ next.config.js
@@ -11,6 +11,81 @@ const nextConfig = {
   compiler: {
     removeConsole: isProd ? { exclude: ['error', 'warn'] } : false,
   },
+
+  async redirects() {
+    return [
+      // Redirect /felicitaciones to /tcp for encuentrocanning.org
+      {
+        source: '/felicitaciones',
+        has: [
+          {
+            type: 'host',
+            value: 'encuentrocanning.org',
+          },
+        ],
+        destination: '/tcp',
+        permanent: true,
+      },
+      {
+        source: '/felicitaciones',
+        has: [
+          {
+            type: 'host',
+            value: 'www.encuentrocanning.org',
+          },
+        ],
+        destination: '/tcp',
+        permanent: true,
+      },
+      // Redirect /tcp/felicitaciones to /tcp for encuentrocanning.org
+      {
+        source: '/tcp/felicitaciones',
+        has: [
+          {
+            type: 'host',
+            value: 'encuentrocanning.org',
+          },
+        ],
+        destination: '/tcp',
+        permanent: true,
+      },
+      {
+        source: '/tcp/felicitaciones',
+        has: [
+          {
+            type: 'host',
+            value: 'www.encuentrocanning.org',
+          },
+        ],
+        destination: '/tcp',
+        permanent: true,
+      },
+      // Redirect /felicitaciones to / for tcp.emubaescuela.com
+      {
+        source: '/felicitaciones',
+        has: [
+          {
+            type: 'host',
+            value: 'tcp.emubaescuela.com',
+          },
+        ],
+        destination: '/',
+        permanent: true,
+      },
+      // Redirect /tcp/felicitaciones to / for tcp.emubaescuela.com
+      {
+        source: '/tcp/felicitaciones',
+        has: [
+          {
+            type: 'host',
+            value: 'tcp.emubaescuela.com',
+          },
+        ],
+        destination: '/',
+        permanent: true,
+      },
+    ]
+  },
 }
  
 module.exports = nextConfig
